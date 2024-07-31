3eb3a8bceba77842f98a13667134999772fa369f
Kirill Krasnobayev <kkrasnobayev@napc.com>
2024-07-31 13:38:46 +0300

    commit all files

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
+    selector: 'app-home2',
     standalone: true,
     imports: [],
     templateUrl: './home.page.html',

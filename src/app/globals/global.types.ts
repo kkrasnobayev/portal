119216daf90710c87222b75f47027724d7ab4e62
Kirill Krasnobayev <kkrasnobayev@napc.com>
2024-07-31 15:48:43 +0300

    allow any for GenericCollection

--- src/app/globals/global.types.ts
+++ src/app/globals/global.types.ts
@@ -11,6 +11,10 @@ export interface Environment {
     redirectUri?: string;
 }
 
+/*
+ * "any" is required here because as a standard GenericCollection consists of key->value pairs with disabled typing for value
+ * GenericCollection without typing should be used only when we don't know or care about actual typing
+ */
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export type GenericCollection<T = any> = Record<string, T>;
 export type Nullable<T> = T | null;
